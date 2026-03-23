1014b708dfa0791943f04c1c1cb85d4c4df3745e
Lena Heisel <104693153+lenalarissa@users.noreply.github.com>
2026-03-23 14:29:19 +0100

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -7,8 +8,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  base: '',
   plugins: [
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
